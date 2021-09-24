5c8f64dfd461a720953755a03a306c221333f30e
lehaidang <H-SKY@DESKTOP-NBVSKA9>
2021-09-24 14:36:03 +0700

    Add project files.

---
+++ LTQL1y-master/LTQL11/Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTQL11.Models;
+
+namespace LTQL11.Controllers
+{
+    public class StudentsController : Controller
+    {
+        LaptrinhquanlyDBcontext db = new LaptrinhquanlyDBcontext();
+        // GET: Students
+        public ActionResult Index()
+        {
+            var model = db.Students.ToList();
+            return View(model);
+        }
+        //tao action create tra ve vuew cho phep ng dung nhap thong tin de them moi
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student std)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Students.Add(std);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+
+}
---
+++ LTQL1y-master/LTQL11/Models/LaptrinhquanlyDBcontext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LTQL11.Models
+{
+    public partial class LaptrinhquanlyDBcontext : DbContext
+    {
+        public LaptrinhquanlyDBcontext()
+            : base("name=LaptrinhquanlyDBcontext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Account>()
+              .Property(e => e.Usename)
+              .IsUnicode(false);
+        }
+    }
+}
